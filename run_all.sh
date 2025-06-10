026f8dc82151304385f6ec968a49a4a49a1ab3ed
Christina Francis <CFranc22@jhu.edu>
2025-06-10 18:46:02 -0400

    updating primary bash script for execution

--- run_all.sh
+++ run_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e  # Exit if any command fails
 
-echo "Running NWS data download script..."
+echo "Running NWS data downloader script..."
 python -m scripts.main_download_nws
 
-echo "Running Stargazing Index script..."
+echo "Running Stargazing Index calculator script..."
 python -m scripts.main_stargazing_index
