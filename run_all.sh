596429f654b438723a29e57036f8060c279fb089
Christina Francis <67291164+christina-k-francis@users.noreply.github.com>
2025-06-15 20:45:27 -0400

    testing scripts

--- run_all.sh
+++ run_all.sh
@@ -2,7 +2,7 @@
 set -e  # Exit if any command fails
 
 echo "Running NWS data downloader script..."
-#python -m scripts.main_nws_download
+python -m scripts.main_nws_download
 
 echo "Running Stargazing Index calculator script..."
 python -m scripts.main_stargazing_index
