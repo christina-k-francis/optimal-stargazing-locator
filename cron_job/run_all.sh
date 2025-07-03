f5aedd322fa94da29e3b3770a996a3c3e196c2b4
Christina Francis <67291164+christina-k-francis@users.noreply.github.com>
2025-07-02 20:32:05 -0400

    Testing converting stargazing ds to tiles

--- cron_job/run_all.sh
+++ cron_job/run_all.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -e  # Exit if any command fails
 
-echo "Running NWS data downloader script..."
-python -m scripts.main_nws_download
+#echo "Running NWS data downloader script..."
+#python -m scripts.main_nws_download
 
 echo "Running Stargazing Index calculator script..."
 python -m scripts.main_stargazing_index
-
-#echo "Testing uploading small zarr..."
-#python -m scripts.Upload_Test
