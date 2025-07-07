1232bb03aab7d998d3f50adc53e39aba72c7de1d
Christina Francis <CFranc22@jhu.edu>
2025-07-07 10:30:38 -0400

    reduce zoom levels, increase sleep time

--- cron_job/scripts/main_nws_download.py
+++ cron_job/scripts/main_nws_download.py
@@ -181,9 +181,6 @@ def generate_tiles_from_zarr(ds, layer_name, supabase_prefix, sleep_secs):
             dx = slice_2d.attrs["GRIB_DxInMetres"]  # Grid spacing in meters (x)
             dy = slice_2d.attrs["GRIB_DyInMetres"]  # Grid spacing in meters (y)
 
-            nx = slice_2d.sizes["x"]
-            ny = slice_2d.sizes["y"]
-
             # Bounds from GRIB metadata
             minx = -2764474.3507319926
             maxy = 3232111.7107923944
@@ -220,7 +217,7 @@ def generate_tiles_from_zarr(ds, layer_name, supabase_prefix, sleep_secs):
 
             # Generate tiles with gdal2tiles
             subprocess.run([
-                "gdal2tiles.py", "-z", "0-10", str(vrt_path), str(tile_output_dir)
+                "gdal2tiles.py", "-z", "0-8", str(vrt_path), str(tile_output_dir)
             ], check=True)
 
             # Upload tiles to Supabase
@@ -266,7 +263,7 @@ def main_download_nws():
     ds=skycover_ds,
     layer_name="cloud_coverage",
     supabase_prefix="data-layer-tiles/SkyCover_Tiles",
-    sleep_secs=0.03) # 30 ms
+    sleep_secs=0.04) # 30 ms
     log_memory_usage("After creating tiles for each timestep")
     del skycover_ds
     gc.collect() # garbage collector. deletes objects that are no longer in use
