c6c2bbd7cd4f26b29008880a63e4c548b84f3745
Christina Francis <67291164+christina-k-francis@users.noreply.github.com>
2025-11-04 21:01:52 -0500

    Deleted no longer needed render directory

--- render/run_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e  # Exit if any command fails
-
-echo "Running NWS data downloader script..."
-python -m scripts.main_nws_download
-
-echo "Running Stargazing Index/Grade Calculator script..."
-python -m scripts.main_stargazing_calc
--- render/scripts/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun May 18 13:38:20 2025
-
-@author: Chris
-"""
-
